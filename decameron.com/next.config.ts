346bef8a58e44daac540b24b1e4ea45ec1a814e4
Jhonatan Jair Garcia Alfaro <joga47uk@gmail.com>
2025-05-13 07:45:00 -0500

    change update data for netlify

--- decameron.com/next.config.ts
+++ decameron.com/next.config.ts
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
-}
+  output: "export",
+};
 
-module.exports = nextConfig
+export default nextConfig;
